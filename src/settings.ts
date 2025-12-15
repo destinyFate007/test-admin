d5debb7009684d850dc9baf1d16d6d5f5bd83ff9
destinyFate007 <qq2538381255@gmail.com>
2025-12-16 07:30:01 +0800

    feat: init

---
+++ src/settings.ts
@@ -0,0 +1,59 @@
+import type { RecursiveRequired, Settings } from '#/global'
+import { cloneDeep } from 'es-toolkit'
+import settingsDefault from '@/settings.default'
+import { merge } from '@/utils/object'
+
+const globalSettings: Settings.all = {
+  app: {
+    enablePermission: true,
+    enableDynamicTitle: true,
+    enableFeedback: true,
+    enableErrorLog: true,
+    enableCheckUpdates: true,
+  },
+  userPreferences: {
+    enable: true,
+  },
+  layout: {
+    enableMobileAdaptation: true,
+  },
+  menu: {
+    style: 'dot',
+    enableSubMenuCollapseButton: true,
+    enableHotkeys: true,
+  },
+  topbar: {
+    mode: 'fixed',
+  },
+  tabbar: {
+    enable: true,
+    style: 'fashion',
+    enableIcon: true,
+    enableMemory: true,
+    enableHotkeys: true,
+  },
+  toolbar: {
+    favorites: true,
+    notification: true,
+    i18n: true,
+    fullscreen: true,
+    pageReload: true,
+    colorScheme: true,
+  },
+  breadcrumb: {
+    style: 'modern',
+    enableMainMenu: true,
+  },
+  mainPage: {
+    iframeCacheMax: 9,
+    transitionMode: 'slide-right',
+  },
+  copyright: {
+    enable: true,
+    dates: '2020-present',
+    company: 'Fantastic-admin',
+    website: 'https://fantastic-admin.hurui.me',
+  },
+}
+
+export default merge(globalSettings, cloneDeep(settingsDefault)) as RecursiveRequired<Settings.all>
